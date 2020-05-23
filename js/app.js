6137bd076317431ae1aaee356bc405aed7c73b3d
SunnyVakil <sunnyshh45@gmail.com>
2020-05-23 17:21:40 -0400

    added navbar style & lightbox jquery plugin

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const navabr = document.querySelector('.navbar');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+  if(window.scrollY > navabr.offsetHeight + 100) {
+    navabr.classList.remove('top');
+  } else {
+    navabr.classList.add('top')
+  }
+}
